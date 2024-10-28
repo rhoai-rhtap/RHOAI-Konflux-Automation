92dfcbb1fb92accfcb65693079174e80f28d4d65
Deepak Chourasia <dchouras@redhat.com>
2024-10-28 16:08:55 +0530

    finalizing tracer

--- tools/tracer.sh
+++ tools/tracer.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 export > /dev/null 2>&1
 
-#quay RO token
-#take rhoai version as input
-#fetch latest image for given version
-#nightly flag to pull nightly info
-#take image as input as well
-#print commits format
-#print http format
-
 RBC_REPO=https://github.com/red-hat-data-services/RHOAI-Build-Config
 BUILD_TYPE=ci
 IMAGE_TYPE=fbc
@@ -22,6 +14,8 @@ IMAGE=
 CONFIGURE=
 UPDATE=
 IMAGE_URI=
+FULL_IMAGE_URI_WITH_DIGEST=
+TEXT_OUTPUT=
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]; do
@@ -46,9 +40,8 @@ while [[ $# -gt 0 ]]; do
         shift
         shift
         ;;
-        --image-type | -it)
-        IMAGE_TYPE="$2"
-        shift
+        --bundle | -b)
+        IMAGE_TYPE=bundle
         shift
         ;;
         --image | -i)
@@ -91,24 +84,69 @@ then
 
   if [[ -n $DIGEST ]]
   then
-    if [[ "$DIGEST" != "sha256*" ]]; then DIGEST="sha256:${DIGEST}"; fi
+    if [[ "$DIGEST" != sha256* ]]; then DIGEST="sha256:${DIGEST}"; fi
     IMAGE_MANIFEST="@$DIGEST"
   elif [[ -n $TAG ]]
   then
     #TAG=$(echo $TAG | tr '[a-z]' '[A-Z]')
     if [[ "$TAG" == v* ]]; then TAG=$(echo $TAG | tr -d 'v'); fi
     if [[ "$TAG" != rhoai* ]]; then TAG="rhoai-${TAG}"; fi
+    if [[ "$BUILD_TYPE" == "NIGHTLY" ]]; then TAG="${TAG}-nightly"; echo $TAG; fi
     IMAGE_MANIFEST=":$TAG"
   fi
   if [[ $IMAGE_TYPE == "FBC" ]]; then QUAY_REPO=$FBC_QUAY_REPO; elif [[ $IMAGE_TYPE == "BUNDLE" ]]; then QUAY_REPO=$BUNDLE_QUAY_REPO; fi
 
   IMAGE_URI="${QUAY_BASE_URL}/${QUAY_REPO}${IMAGE_MANIFEST}"
+  FULL_IMAGE_URI_WITH_DIGEST="${QUAY_BASE_URL}/${QUAY_REPO}"
 
 
 else
-  IMAGE_URI=$IMAGE
-  if [[ "$IMAGE_URI" != docker* ]]; then IMAGE_URI="docker://${IMAGE}"; fi
+  IMAGE_URI=${IMAGE/http:\/\//}
+  IMAGE_URI=$(echo $IMAGE_URI | sed -e 's/:rhoai-2.*@/@/g')
+  if [[ "$IMAGE_URI" != docker* ]]; then IMAGE_URI="docker://${IMAGE_URI}"; fi
+  FULL_IMAGE_URI_WITH_DIGEST=$IMAGE_URI
 fi
 
 if [[ -n $IMAGE_URI ]]
-  labels = $(skopeo inspect "${IMAGE_URI}" | jq .Labels)
+then
+  META=$(skopeo inspect "${IMAGE_URI}")
+  NAME=$(echo $META | jq -r .Name)
+  IFS='/' read -a parts <<< "$NAME"
+  CURRENT_COMPONENT="${parts[2]}"
+  DIGEST=$(echo $META | jq -r .Digest)
+
+  labels=$(echo $META | jq .Labels)
+
+  FULL_IMAGE_URI_WITH_DIGEST="${NAME}@${DIGEST}"
+  BUILD_DATE=$(echo $labels | jq -r '."build-date"')
+
+  TEXT_OUTPUT="${TEXT_OUTPUT}Image-URI ${FULL_IMAGE_URI_WITH_DIGEST}\n"
+  TEXT_OUTPUT="${TEXT_OUTPUT}Build-Date ${BUILD_DATE}\n"
+
+  if [[ "$SHOW_COMMITS" == "true" ]]
+  then
+    declare -a COMPONENTS=()
+    while read -r key;
+    do
+      if [[ "$key" == *git.url ]]
+      then
+        if [[ $key == "git.url" ]]; then component="${CURRENT_COMPONENT}"; else component="${key/.git.url/}"; fi
+        COMPONENTS+=($component)
+      fi
+      #echo $key=$(echo $labels | jq  --arg key "$key" -r '"\(.[$key])"')
+    done < <(echo $labels | jq -r "keys[]")
+
+    for component in "${COMPONENTS[@]}"
+    do
+      if [[ $component != "${CURRENT_COMPONENT}" ]]; then url_key="$component.git.url"; commit_key="$component.git.commit"; else url_key="git.url"; commit_key="git.commit"; fi
+      URL=$(echo $labels | jq  --arg url_key "$url_key" -r '"\(.[$url_key])"')
+      COMMIT=$(echo $labels | jq  --arg commit_key "$commit_key" -r '"\(.[$commit_key])"')
+
+      TEXT_OUTPUT="${TEXT_OUTPUT}${component} ${URL}/tree/${COMMIT}\n"
+    done
+  fi
+  echo -e "$TEXT_OUTPUT" | column -t
+
+else
+  echo "Image is not found"
+fi
