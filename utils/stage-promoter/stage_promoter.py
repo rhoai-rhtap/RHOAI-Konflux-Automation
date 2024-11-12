5f8962c3c2245420bf3d8b88152806b203217cb4
Deepak Chourasia <dchouras@redhat.com>
2024-11-13 00:29:48 +0530

    updating stage promoter

--- utils/stage-promoter/stage_promoter.py
+++ utils/stage-promoter/stage_promoter.py
@@ -1,4 +1,6 @@
 import os, requests
+import time
+
 from jsonupdate_ng import jsonupdate_ng
 import argparse
 import yaml
@@ -7,6 +9,8 @@ import json
 from collections import defaultdict
 from ruamel.yaml.scalarstring import DoubleQuotedScalarString
 import base64
+import sys
+
 class stage_promoter:
     PRODUCTION_REGISTRY = 'registry.redhat.io'
     PACKAGE_NAME = 'rhods-operator'
@@ -85,6 +89,95 @@ class stage_promoter:
     def patch_olm_bundles(self):
         self.patch_current_release_bundle_schema()
 
+class snapshot_processor:
+    GIT_URL_LABEL_KEY = 'git.url'
+    GIT_COMMIT_LABEL_KEY = 'git.commit'
+    FBC_FRAGMENT_REPO = 'quay.io/rhoai/rhoai-fbc-fragment'
+    def __init__(self, rhoai_version:str, build_config_path:str, timeout:str, output_file_path:str, git_commit:str):
+        self.output_file_path = output_file_path
+        self.rhoai_version = rhoai_version
+        self.build_config_path = build_config_path
+        self.build_config = yaml.safe_load(open(self.build_config_path))
+        self.ocp_versions_for_release = self.build_config['config']['supported-ocp-versions']['release']
+        self.timeout = int(timeout * 60)
+        self.git_commit = git_commit
+
+    def monitor_fbc_builds(self):
+        fbc_images = {}
+        qc = quay_controller('rhoai')
+        time_lapsed = 0
+        def all_fbc_builds_finished():
+            all_versions_covered = True
+            for ocp_version in self.ocp_versions_for_release:
+                if ocp_version not in fbc_images:
+                    all_versions_covered = False
+                    break
+            return all_versions_covered
+
+        while not all_fbc_builds_finished() and time_lapsed < self.timeout:
+            for ocp_version in self.ocp_versions_for_release:
+                fbc_image_tag = f'ocp-{ocp_version.strip("v")}-{self.rhoai_version}-{self.git_commit}'
+                tags = qc.get_all_tags(self.FBC_FRAGMENT_REPO, fbc_image_tag)
+                if not tags:
+                    print(f'no tags found for {fbc_image_tag}, waiting..')
+                for tag in tags:
+                    sig_tag = f'{tag["manifest_digest"].replace(":", "-")}.sig'
+                    signature = qc.get_tag_details(self.FBC_FRAGMENT_REPO, sig_tag)
+                    if signature:
+                        fbc_images[ocp_version] = f'{self.FBC_FRAGMENT_REPO}@{tag["manifest_digest"]}'
+            time.sleep(45)
+            time_lapsed += 45
+
+            missing_images = []
+            for ocp_version in self.ocp_versions_for_release:
+                if ocp_version not in fbc_images:
+                    fbc_images[ocp_version] = 'NOT_FOUND'
+                    missing_images.append(ocp_version)
+
+            json.dumps(fbc_images, open(self.output_file_path, 'w'))
+            if missing_images:
+                print('FBC images not found for following OCP versions - ', missing_images)
+                sys.exit(1)
+
+
+
+
+
+
+BASE_URL = 'https://quay.io/api/v1'
+class quay_controller:
+    def __init__(self, org:str):
+        self.org = org
+    def get_tag_details(self, repo, tag):
+        result_tag = {}
+        url = f'{BASE_URL}/repository/{self.org}/{repo}/tag/?specificTag={tag}&onlyActiveTags=true'
+        headers = {'Authorization': f'Bearer {os.environ[self.org.upper() + "_QUAY_API_TOKEN"]}',
+                   'Accept': 'application/json'}
+        response = requests.get(url, headers=headers)
+        tags = response.json()['tags']
+        if tags:
+            result_tag = tags[0]
+        return result_tag
+    def get_all_tags(self, repo, tag):
+        url = f'{BASE_URL}/repository/{self.org}/{repo}/tag/?specificTag={tag}&onlyActiveTags=false'
+        headers = {'Authorization': f'Bearer {os.environ[self.org.upper() + "_QUAY_API_TOKEN"]}',
+                   'Accept': 'application/json'}
+        response = requests.get(url, headers=headers)
+        tag = response.json()['tags']
+        return tag
+
+    def get_git_labels(self, repo, tag):
+        url = f'{BASE_URL}/repository/{self.org}/{repo}/manifest/{tag}/labels?filter=git'
+        headers = {'Authorization': f'Bearer {os.environ[self.org.upper() + "_QUAY_API_TOKEN"]}',
+                   'Accept': 'application/json'}
+        response = requests.get(url, headers=headers)
+        if 'labels' in response.json():
+            labels = response.json()['labels']
+            return labels
+        else:
+            print(response.json())
+            sys.exit(1)
+
 def str_presenter(dumper, data):
     if data.count('\n') > 0:
         return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
@@ -95,7 +188,7 @@ def str_presenter(dumper, data):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-op', '--operation', required=False,
-                        help='Operation code, supported values are "catalog-patch" and "extract-snapshot-images"', dest='operation')
+                        help='Operation code, supported values are "stage-catalog-patch" and "monitor-fbc-builds"', dest='operation')
     parser.add_argument('-c', '--catalog-yaml-path', required=False,
                         help='Path of the catalog.yaml from the main branch.', dest='catalog_yaml_path')
     parser.add_argument('-p', '--patch-yaml-path', required=False,
@@ -106,11 +199,20 @@ if __name__ == '__main__':
                         help='Path of the output catalog yaml', dest='output_file_path')
     parser.add_argument('-v', '--rhoai-version', required=False,
                         help='The version of Openshift-AI being processed', dest='rhoai_version')
+    parser.add_argument('-t', '--timeout', required=False,
+                        help='Timeout while waiting for FBC builds to finish', dest='timeout')
+    parser.add_argument('-b', '--build-config-path', required=False,
+                        help='Path of the build-config.yaml', dest='build_config_path')
+    parser.add_argument('-g', '--git-commit', required=False,
+                        help='expected git.commit of the FBC images', dest='git_commit')
     args = parser.parse_args()
 
     if args.operation.lower() == 'stage-catalog-patch':
         promoter = stage_promoter(catalog_yaml_path=args.catalog_yaml_path, patch_yaml_path=args.patch_yaml_path, release_catalog_yaml_path=args.release_catalog_yaml_path, output_file_path=args.output_file_path, rhoai_version=args.rhoai_version)
         promoter.patch_catalog_yaml()
+    elif args.operation.lower() == 'monitor-fbc-builds':
+        processor = snapshot_processor(rhoai_version=args.rhoai_version, build_config_path=args.build_config_path, timeout=args.timeout, output_file_path=args.output_file_path, git_commit=args.git_commit)
+        processor.monitor_fbc_builds()
 
     # c = '/home/dchouras/RHODS/DevOps/FBC/main/catalog/v4.13/rhods-operator/catalog.yaml'
     # p = '/home/dchouras/RHODS/DevOps/RHOAI-Build-Config/catalog/catalog-patch.yaml'
